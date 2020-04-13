0931028561e71f262f5f8b5856c3e5d3331b68da
Nazar Chabara <63397396+nazar-developer@users.noreply.github.com>
2020-04-13 12:05:11 +0200

    Added configurations file and implemented separate developer menu

--- applications/accounting/models/menu.py
+++ applications/accounting/models/menu.py
@@ -1,5 +1,20 @@
+
 if not configuration.get('app.production'):
-    _app = request.application
+    response.menu += [
+        (T('Manage Account'), False, '#', [
+            (T('Create Account'), False, URL('accounting', 'account',
+                                             'create_account')),
+            (T('Add Sector'), False, URL('accounting', 'account',
+                                         'add_sector'))]),
+        (T('Income'), False, URL('accounting', 'account', 'income')),
+        (T('Outcome'), False, URL('accounting', 'account', 'outcome')),
+        (T('Balance'), False, URL('accounting', 'account', 'balance')),
+        (T('Dev Tools'), False, '#', [
+            (T('Insert Test Data'), False, URL('accounting', 'dev_tools',
+                                               'insert_test_data'))]),
+    ]
+
+if configuration.get('app.production'):
     response.menu += [
         (T('Manage Account'), False, '#', [
             (T('Create Account'), False, URL('accounting', 'account',
