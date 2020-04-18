531da996fe7e1b0c43bc9e77a886c7e01dc98487
Nazar Chabara <63397396+nazar-developer@users.noreply.github.com>
2020-04-18 13:40:11 +0200

    Added auth.sinature which adds additional fields like modified_by, modified_on  to the tables.

--- applications/accounting/models/db_tables.py
+++ applications/accounting/models/db_tables.py
@@ -1,9 +1,16 @@
+from gluon.tools import Auth
+auth = Auth(db)
+auth.enable_record_versioning(db)
+auth.define_tables(signature=True)
+
 db.define_table('account',
                 Field(fieldname='account_name', type='string'),
+                auth.signature,
                 format='%(account_name)s')
 
 db.define_table('sector',
                 Field(fieldname='sector_type', type='string'),
+                auth.signature,
                 format='%(sector_type)s')
 
 db.define_table('income',
@@ -11,18 +18,20 @@ db.define_table('income',
                 Field(fieldname='sector_type_id',
                       type='reference sector'),
                 Field(fieldname='income_date', type='datetime'),
-                Field(fieldname='amount', type='double'))
+                Field(fieldname='amount', type='double'),
+                auth.signature)
 
 db.define_table('outcome',
                 Field(fieldname='account_id', type='reference account'),
                 Field(fieldname='sector_type_id',
                       type='reference sector'),
                 Field(fieldname='income_date', type='datetime'),
-                Field(fieldname='amount', type='double'))
+                Field(fieldname='amount', type='double'),
+                auth.signature)
 
 db.define_table('balance',
                 Field(fieldname='account_id', type='reference account'),
                 Field(fieldname='amount', type='double', readable=True,
-                      writable=True))
-
+                      writable=True),
+                auth.signature)
 
