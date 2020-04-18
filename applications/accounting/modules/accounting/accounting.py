d469f93e6697f4ed8e88761bdf9366aa313db57a
Nazar Chabara <63397396+nazar-developer@users.noreply.github.com>
2020-04-18 13:48:59 +0200

    Added 3 new attributes to the Account class  and  two new methods
    extract_amount: extracts amount from a list of dictionaries
    sum_up_amount: sums up list of amounts and returns total_amount

--- applications/accounting/modules/accounting/accounting.py
+++ applications/accounting/modules/accounting/accounting.py
@@ -6,14 +6,26 @@ class Accounting:
 class Account(Accounting):
 
     def __init__(self, account_id=None, account_name=None, sector_id=None,
-                 sector_type=None, creation_date=None, amount=None):
+                 sector_type=None, creation_date=None, amount=None,
+                 total_balance=None):
 
-        self.account_id = account_id
+        self._account_id = account_id
         self._account_name = account_name
-        self.sector_type_id = sector_id
+        self._sector_type_id = sector_id
         self._sector_type = sector_type
         self._creation_date = creation_date
         self._amount = amount
+        self._total_balance = total_balance
+
+    @property
+    def account_id(self):
+        return self._account_id
+
+    @account_id.setter
+    def account_id(self, account_id):
+        if not account_id:
+            raise ValueError('Error! account_id ')
+        self._account_id = account_id
 
     @property
     def account_name(self):
@@ -24,6 +36,16 @@ class Account(Accounting):
         if not account_name:
             raise ValueError('Error! account_name ')
         self._account_name = account_name
+
+    @property
+    def sector_type_id(self):
+        return self._sector_type_id
+
+    @sector_type_id.setter
+    def sector_type_id(self, sector_type_id):
+        if not sector_type_id:
+            raise ValueError('Error! sector_type_id')
+        self._sector_type_id = sector_type_id
 
     @property
     def sector_type(self):
@@ -54,3 +76,27 @@ class Account(Accounting):
         if not amount:
             raise ValueError('Error! amount')
         self._amount = amount
+
+    @property
+    def total_balance(self):
+        return self._balance
+
+    @total_balance.setter
+    def total_balance(self, total_balance):
+        if not total_balance:
+            raise ValueError('Error!', total_balance)
+        self._total_balance = total_balance
+
+    @staticmethod
+    def extract_amount(values, key):
+        amounts = []
+        for value in values:
+            amounts.append(value[key])
+        return amounts
+
+    @staticmethod
+    def sum_up_amount(amounts):
+        totalized_amount = 0.0
+        for amount in amounts:
+            totalized_amount += amount
+        return totalized_amount
