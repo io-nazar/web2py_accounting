ba77a01d996e9ecbf3b176cb677b645195f3c9b9
Nazar Chabara <63397396+nazar-developer@users.noreply.github.com>
2020-04-26 14:15:19 +0200

    Added requires IS_NOT_EMPTY to the db tables
    This implementation avoids creation of empty records without data

--- applications/accounting/models/db_tables.py
+++ applications/accounting/models/db_tables.py
@@ -4,39 +4,49 @@ auth.enable_record_versioning(db)
 auth.define_tables(signature=True)
 
 db.define_table('account',
-                Field(fieldname='account_name', type='string'),
+                Field(fieldname='account_name', type='string',
+                      requires=IS_NOT_EMPTY()),
                 auth.signature,
                 format='%(account_name)s')
 
 db.define_table('sector',
-                Field(fieldname='sector_type', type='string'),
+                Field(fieldname='sector_type', type='string',
+                      requires=IS_NOT_EMPTY()),
                 auth.signature,
                 format='%(sector_type)s')
 
 db.define_table('income',
-                Field(fieldname='account_id', type='reference account'),
-                Field(fieldname='sector_type_id',
-                      type='reference sector'),
-                Field(fieldname='income_date', type='datetime'),
-                Field(fieldname='amount', type='double'),
+                Field(fieldname='account_id', type='reference account',
+                      requires=IS_NOT_EMPTY()),
+                Field(fieldname='sector_type_id',type='reference sector',
+                      requires=IS_NOT_EMPTY()),
+                Field(fieldname='income_date', type='datetime',
+                      requires=IS_NOT_EMPTY()),
+                Field(fieldname='amount', type='double',
+                      requires=IS_NOT_EMPTY()),
                 Field(fieldname='comment_field', type='text', label='Comment'),
                 auth.signature,
                 format='%(amount)s')
 
 db.define_table('outgoing',
-                Field(fieldname='account_id', type='reference account'),
-                Field(fieldname='sector_type_id',
-                      type='reference sector'),
-                Field(fieldname='income_date', type='datetime'),
-                Field(fieldname='amount', type='double'),
-                Field(fieldname='comment_field', type='text', label='Comment'),
+                Field(fieldname='account_id', type='reference account',
+                      requires=IS_NOT_EMPTY()),
+                Field(fieldname='sector_type_id', type='reference sector',
+                      requires=IS_NOT_EMPTY()),
+                Field(fieldname='income_date', type='datetime',
+                      requires=IS_NOT_EMPTY()),
+                Field(fieldname='amount', type='double',
+                      requires=IS_NOT_EMPTY()),
+                Field(fieldname='comment_field', type='text',
+                      requires=IS_NOT_EMPTY(), label='Comment'),
                 auth.signature,
                 format='%(amount)s')
 
 db.define_table('balance',
-                Field(fieldname='account_id', type='reference account'),
+                Field(fieldname='account_id', type='reference account',
+                      requires=IS_NOT_EMPTY()),
                 Field(fieldname='amount', type='double', readable=True,
-                      writable=True),
+                      writable=True, requires=IS_NOT_EMPTY()),
                 auth.signature,
                 format='%(amount)s')
 
