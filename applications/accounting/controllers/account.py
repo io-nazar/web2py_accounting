d3505ea6cfd9493f6644b45375ab8aa5d7cfa14b
Nazar Chabara <63397396+nazar-developer@users.noreply.github.com>
2020-06-11 12:10:13 +0200

    Added new dict attribute in to PieChart class
    pie_chart_conf attribute: this attribute includes configuration for Pie Chart

--- applications/accounting/controllers/account.py
+++ applications/accounting/controllers/account.py
@@ -192,6 +192,8 @@ def get_msg(msg_type, msg_str):
 def draw_plot():
     pie_chart = PieChart()
     pie_chart.pie_chart_data = dict(Other=300, Dividend=1100.5, Salary=20000.1)
+    pie_chart.pie_chart_conf = dict(xrange=(-1, 1.0), plot_height=500, x=0, y=1,
+                                    radius=0.4)
     pie_chart.chart_title = "Pie Chart: Category / Amount"
     plot_html = pie_chart.create_pie_chart()
     return plot_html
--- applications/accounting/modules/application/plots/charts/charts.py
+++ applications/accounting/modules/application/plots/charts/charts.py
@@ -29,6 +29,7 @@ class PieChart(BaseChart):
     def __init__(self):
         super(PieChart, self).__init__(self)
         self._pie_chart_data = dict()
+        self._pie_chart_conf = dict()
 
     @property
     def pie_chart_data(self):
@@ -41,19 +42,32 @@ class PieChart(BaseChart):
         else:
             raise Exception('Error pie_chart_data')
 
-    def create_pie_chart(self):
+    @property
+    def pie_chart_conf(self):
+        return self._pie_chart_conf
 
+    @pie_chart_conf.setter
+    def pie_chart_conf(self, pie_chart_conf):
+        if isinstance(pie_chart_conf, dict):
+            self._pie_chart_conf = pie_chart_conf
+        else:
+            raise Exception('Error pie_chart_conf')
+
+    def create_pie_chart(self):
         test_data = self._pie_chart_data
         data = pd.Series(test_data).reset_index(name='value').rename(
             columns={'index': 'category'})
         data['angle'] = data['value'] / data['value'].sum() * 2 * pi
         data['color'] = Category20c[len(test_data)]
-        p = figure(plot_height=500, title=self.chart_title,
+
+        p = figure(plot_height=self.pie_chart_conf['plot_height'],
+                   title=self.chart_title,
                    toolbar_location=None,
                    tools="hover", tooltips="@category: @value",
-                   x_range=(-1, 1.0))
+                   x_range=self.pie_chart_conf['xrange'])
 
-        p.wedge(x=0, y=1, radius=0.4,
+        p.wedge(x=self.pie_chart_conf['x'], y=self.pie_chart_conf['y'],
+                radius=self.pie_chart_conf['radius'],
                 start_angle=cumsum('angle', include_zero=True),
                 end_angle=cumsum('angle'),
                 line_color="white", fill_color='color', legend_field='category',
