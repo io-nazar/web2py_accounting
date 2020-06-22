bc2ae15c9024761746d025ae89f928fa2cbf76ce
Nazar Chabara <63397396+nazar-developer@users.noreply.github.com>
2020-06-23 01:12:35 +0200

    Adjusted draw_plot controller method

--- applications/accounting/controllers/account.py
+++ applications/accounting/controllers/account.py
@@ -189,11 +189,12 @@ def get_msg(msg_type, msg_str):
 
 
 @auth.requires_login()
-def draw_plot():
-    pie_chart = PieChart()
-    pie_chart.pie_chart_data = dict(Other=300, Dividend=1100.5, Salary=20000.1)
-    pie_chart.pie_chart_conf = dict(xrange=(-1, 1.0), plot_height=500, x=0, y=1,
-                                    radius=0.4)
+def draw_plot(plot_data, plot_type):
+    if plot_type == 'PieChart':
+        pie_chart = PieChart()
+        pie_chart.pie_chart_data = plot_data
+        pie_chart.pie_chart_conf = dict(xrange=(-1, 1.0), plot_height=500,
+                                        x=0, y=1, radius=0.4)
     pie_chart.chart_title = "Pie Chart: Category / Amount"
     plot_html = pie_chart.create_pie_chart()
     return plot_html
